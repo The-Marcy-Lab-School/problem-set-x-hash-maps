7dfd2773bde0cc04a95ded7a16b766657bfc1569
Ann Duong <thuyanannduong@gmail.com>
2021-02-05 11:06:02 -0500

    added questions

--- problems/exercises.test.js
+++ problems/exercises.test.js
@@ -1,7 +1,66 @@
-const { } = require('./exercises');
+const { singleNumber, canConstruct, hasDuplicates } = require('./exercises');
 
-describe.skip("Test", () => {
-  it("passes", () => {
-    expect(true).toEqual(true)
+describe("singleNumber", () => {
+  it("finds the single number in a short array", () => {
+    expect(singleNumber([2,2,1])).toEqual(1)
+  })
+  
+  it("finds the single number in a long array", () => {
+    expect(singleNumber([4,1,2,1,2])).toEqual(4)
+  })
+
+  it("finds the single number in a single element array", () => {
+    expect(singleNumber([1])).toEqual(1)
+  })
+})
+
+describe("canConstruct", () => {
+  it("returns false if letter doesn't exist in magazine", () => {
+    expect(canConstruct("a", "bcdefg")).toEqual(false)
+  })
+  
+  it("returns false if not enough letters appear in magazine", () => {
+    expect(canConstruct("aa", "abcdefg")).toEqual(false)
+  })
+
+  it("returns true if enough letters exist in magazine", () => {
+    expect(canConstruct("aab", "aaabcdefg")).toEqual(true)
+  })
+
+  it("returns false if letter doesn't exist in magazine", () => {
+    expect(canConstruct("aaz", "aabcdefg")).toEqual(false)
+  })
+})
+
+describe("hasDuplicates", () => {
+  it("returns true if there is a duplicate", () => {
+    let name = ["ball", "bat", "glove", "glove", "glove"]
+    let price = [2, 3, 1, 2, 1]
+    let weight = [2, 5, 1, 1, 1]
+    expect(hasDuplicates(name, price, weight)).toEqual(true)
+  })
+  
+  it("returns false if there are no duplicates", () => {
+    let name = ["ball", "bat", "glove", "glove", "glove"]
+    let price = [2, 3, 1, 2, 2]
+    let weight = [2, 5, 1, 1, 2]
+    expect(hasDuplicates(name, price, weight)).toEqual(false)
+  })
+
+  it("returns false if there are no duplicates (check hash function)", () => {
+    let name = ["ball", "bat", "glove", "glove", "glove"]
+    let price = [2, 3, 1, 22, 2]
+    let weight = [2, 5, 1, 2, 22]
+    expect(hasDuplicates(name, price, weight)).toEqual(false)
+
+    name = ["ball", "bat", "glove", "2glove", "glove2"]
+    price = [2, 3, 1, 2, 22]
+    weight = [2, 5, 1, 22, 2]
+    expect(hasDuplicates(name, price, weight)).toEqual(false)
+
+    name = ["ball", "bat", "glove", "glove2", "2glove"]
+    price = [2, 3, 1, 2, 22]
+    weight = [2, 5, 1, 22, 2]
+    expect(hasDuplicates(name, price, weight)).toEqual(false)
   })
 })
--- short-reponse.md
+++ short-reponse.md
@@ -1,7 +1,3 @@
 # Short Reponse Questions
 
-**1. Question**
-
-**2. Question**
-
-**3. Question**
+**1. What is a Hash Map and how does it work? Be sure to mention the terms "key", "value", and "hash function" in your explanation.**
